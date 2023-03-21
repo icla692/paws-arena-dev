250f0794bb9a2a8065ed30bdd9b09a2e29a12e40
Dragos Silion <silidragos@gmail.com>
2023-03-21 19:53:39 +0200

    dev photon

--- Assets/_ProjectAssets/Scripts/ConfigurationModule/ScriptableObjects/GameConfig.cs
+++ Assets/_ProjectAssets/Scripts/ConfigurationModule/ScriptableObjects/GameConfig.cs
@@ -18,4 +18,7 @@ public class GameConfig : ScriptableObject
     public string devUrl = "https://localhost:7226";
     public string stagingUrl = "https://localhost:7226";
     public string prodUrl = "https://localhost:7226";
+
+    public string devPhotonKey = "ea89fb64-713d-4979-b25e-f61a960b4bde";
+    public string prodPhotonKey = "3a4a871a-3614-4f4e-8c0e-43ed06d3d6ed";
 }
