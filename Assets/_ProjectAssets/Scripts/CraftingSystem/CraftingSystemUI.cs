0c1c9ec908d7a64b4ae23a0700fa53d99866db26
filipk00 <filipkrcmarevic555@gmail.com>
2023-04-22 17:47:14 +0200

    Moved missing files from crafting

--- Assets/_ProjectAssets/Scripts/CraftingSystem/CraftingSystemUI.cs
+++ Assets/_ProjectAssets/Scripts/CraftingSystem/CraftingSystemUI.cs
@@ -1,8 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CraftingSystemUI : MonoBehaviour
 {
+    [SerializeField] CraftingUI craftingUI;
+    [SerializeField] DisenchantUI disenchantUI;
 
+    public void Setup()
+    {
+        craftingUI.Setup();
+        disenchantUI.Close();
+    }
+
+    //called from UI buttons
+    public void ShowCraftiongUI()
+    {
+        craftingUI.Setup();
+        disenchantUI.Close();
+    }
+
+
+    //called from UI buttons
+    public void ShowDisenchtUI()
+    {
+        craftingUI.Close();
+        disenchantUI.Setup();
+    }
 }
--- Assets/_ProjectAssets/Scripts/CraftingSystem/DisenchantUI.cs
+++ Assets/_ProjectAssets/Scripts/CraftingSystem/DisenchantUI.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class DisenchantUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Setup()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    }
+    public void Close()
     {
-        
+
     }
 }
